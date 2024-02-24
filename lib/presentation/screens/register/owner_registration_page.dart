f9033af1fb96a3fb74b039d4910f8d85444cdd84
Hans Jurado <hansleonel@gmail.com>
2024-02-24 03:31:17 -0500

    add owner registration that need design

---
+++ lib/presentation/screens/register/owner_registration_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OwnerRegistrationPage extends StatelessWidget {
+  const OwnerRegistrationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
